c972f963dc04d6652056639569a9c9ac687859d9
zeynpkara <163341714+zeynpkara@users.noreply.github.com>
2026-04-27 22:19:48 +0300

    Modify StepPanel constructor to include parameters
    Refactored StepPanel constructor to accept stepTitle and stepNumber parameters, and updated layout setup.

--- SENG272_HW&PROJECT/ISO15939-Simulator/272Project/src/com/isoproject/view/StepPanel.java
+++ SENG272_HW&PROJECT/ISO15939-Simulator/272Project/src/com/isoproject/view/StepPanel.java
@@ -1,19 +1,23 @@
 package com.isoproject.view;
-
 import javax.swing.*;
 import java.awt.*;
 
-
 public abstract class StepPanel extends JPanel {
-
-    public StepPanel() {
-        setBackground(Color.WHITE);
+    public StepPanel(String stepTitle, String stepNumber) {
         setLayout(new BorderLayout());
+        setBackground(Color.WHITE);
+
+        JPanel rightPanel = new JPanel(new BorderLayout());
+        rightPanel.setBackground(new Color(236, 240, 241));
+        rightPanel.setPreferredSize(new Dimension(250, 0));
+
+        JLabel lblStep = new JLabel(stepNumber, JLabel.CENTER);
+        lblStep.setFont(new Font("Segoe UI Black", Font.BOLD, 60));
+        lblStep.setForeground(new Color(189, 195, 199));
+        rightPanel.add(lblStep, BorderLayout.CENTER);
+
+        add(rightPanel, BorderLayout.EAST);
     }
-
-
     public abstract boolean validateInput();
-
-
     public abstract void loadData();
 }
